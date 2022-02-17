dcc2fe60894105ec9136da98016431dd044b748b
DEEPLER <dani2014games@gmail.com>
2022-02-17 14:34:03 -0300

    Terceiro coment

---
+++ Java/MyFirstProgramJava.java
@@ -0,0 +1,17 @@
+//MyFirstProgramJava.java
+
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgramJava {
+
+	public static void main(String[] args) {
+
+		Final Order order = new Order("code1234");;
+		System.out.println(order);
+
+
+	}
+
+}
